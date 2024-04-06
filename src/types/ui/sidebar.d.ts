62fade7dcb5e1dfce0539c41f55d8c31c911a5fc
Tharindu Epasingha <2020cs050@stu.ucsc.cmb.ac.lk>
2024-04-06 18:29:29 +0530

    [defect] : Sidebar and layout : fixed issues and created new ui changes

---
+++ src/types/ui/sidebar.d.ts
@@ -0,0 +1,10 @@
+export interface NavLinkSublink {
+    label: string;
+    href: string;
+  }
+  
+export interface NavLink {
+    label: string;
+    href: string;
+    sublinks?: NavLinkSublink[];
+  }
