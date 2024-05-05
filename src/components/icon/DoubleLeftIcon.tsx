b3254e1231feaf18ee153d9f2e8da741b6e05989
Harsha-Gunawardane <mrgunawardaneofficial@gmail.com>
2024-05-05 22:08:44 +0530

    Make responsive the sidebar & implement vehicle model edit feature

---
+++ src/components/icon/DoubleLeftIcon.tsx
@@ -0,0 +1,26 @@
+import { IconProps } from "@/types/component/propTypes";
+
+function DoubleLeftIcon({ height, width, color }: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      fill="currentColor"
+      className="bi bi-chevron-double-left"
+      viewBox="0 0 16 16"
+      style={{ color: `${color}` }}
+    >
+      <path
+        fill-rule="evenodd"
+        d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"
+      />
+      <path
+        fill-rule="evenodd"
+        d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"
+      />
+    </svg>
+  );
+}
+
+export default DoubleLeftIcon;
