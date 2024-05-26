568ff78d2e81c516ce4109285776ce03cc17ad53
Avishka <baavishka@gmail.com>
2024-05-26 13:17:51 +0530

    feature: add cashInvoice api

--- src/pages/dashboard/invoice/cash/components/AddItem.tsx
+++ src/pages/dashboard/invoice/cash/components/AddItem.tsx
@@ -41,100 +41,6 @@ const AddItem: React.FC = () => {
     },
   });
 
-<<<<<<< HEAD
-  const handleAddItem = () => {
-    if (newItem && newQuantity > 0 && newPrice >= 0) {
-      addItem({
-        name: newItem,
-        quantity: newQuantity,
-        price: newPrice,
-        description: newDescription,
-        discount: newDiscount,
-        code: newCode,
-        // isOutsourced: false,
-      });
-      clearNewItem();
-    }
-  };
-
-  const handleClearItem = () => {
-    clearNewItem();
-  };
-
-  return (
-    <Card>
-      <CardContent className="p-3 shadow-sm bg-slate-200">
-        <div className="grid  gap-4">
-          <div className="grid grid-cols-4 gap-4">
-            <div className="flex flex-col gap-2">
-              <Label>Item Name</Label>
-              <Input
-                type="text"
-                value={newItem}
-                onChange={(e) => setNewItem(e.target.value)}
-                placeholder="Item name"
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <Label>Price</Label>
-              <Input
-                type="number"
-                value={newPrice}
-                onChange={(e) => setNewPrice(parseFloat(e.target.value))}
-                placeholder="Price"
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <Label>Quantity</Label>
-              <Input
-                type="number"
-                value={newQuantity}
-                onChange={(e) => setNewQuantity(parseInt(e.target.value))}
-                placeholder="Quantity"
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <Label>Code</Label>
-              <Input
-                type="text"
-                value={newCode}
-                onChange={(e) => setNewCode(e.target.value)}
-                placeholder="Code"
-              />
-            </div>
-          </div>
-          <div className="grid grid-cols-2 gap-4">
-            <div className="flex flex-col gap-2">
-              <Label>Description</Label>
-              <Input
-                type="text"
-                value={newDescription}
-                onChange={(e) => setNewDescription(e.target.value)}
-                placeholder="Description"
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <Label>Discount (per item)</Label>
-              <Input
-                type="number"
-                value={newDiscount}
-                onChange={(e) => setNewDiscount(parseFloat(e.target.value))}
-                placeholder="Discount"
-              />
-            </div>
-          </div>
-        </div>
-        <div className="flex justify-end gap-4 mt-4">
-          <Button className="" onClick={handleAddItem}>
-            <PlusCircle className="mr-2" /> Add Item
-          </Button>
-          <Button className="bg-slate-500" onClick={handleClearItem}>
-            <XCircle className="mr-2" /> Clear
-          </Button>
-        </div>
-      </CardContent>
-    </Card>
-=======
   const onSubmit = (data: any) => {
     addInvoiceItem(data);
     form.reset()
@@ -236,7 +142,6 @@ const AddItem: React.FC = () => {
           </Form>
         </CardContent>
       </Card>
->>>>>>> 1cd666e3224907648aabf4b9a2bc449ca4a2fdc6
   );
 };
 
