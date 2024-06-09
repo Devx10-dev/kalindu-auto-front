6221bc852c596372b8efaedd4699fc775acf8fa7
Tharindu Epasingha <2020cs050@stu.ucsc.cmb.ac.lk>
2024-06-09 10:42:20 +0530

    creditor register page fixes

--- src/pages/dashboard/creditors/RegisterCreditor.tsx
+++ src/pages/dashboard/creditors/RegisterCreditor.tsx
@@ -1,9 +1,4 @@
-import Loading from "@/components/Loading";
 import { Separator } from "@/components/ui/separator";
-import { ToastAction } from "@/components/ui/toast";
-import { useToast } from "@/components/ui/use-toast";
-import useAxiosPrivate from "@/hooks/usePrivateAxios";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { z } from "zod";
 import { RegisterForm } from "./components/RegisterForm";
 import { creditorFormSchema } from "./components/formScheme";
@@ -11,39 +6,6 @@ import { creditorFormSchema } from "./components/formScheme";
 type CreditorFormValues = z.infer<typeof creditorFormSchema>;
 
 export default function RegisterCreditor() {
-  const axiosPrivate = useAxiosPrivate();
-  const creditorService = new CreditorService(axiosPrivate);
-  const queryClient = useQueryClient();
-  const { toast } = useToast();
-
-  const createCreditorMutation = useMutation({
-    mutationFn: (data: CreditorFormValues) =>
-      creditorService.createCreditor(data),
-    onSuccess: () => {
-      // Handle onSuccess logic here
-      queryClient.invalidateQueries({ queryKey: ["creditors"] });
-      toast({
-        variant: "default",
-        title: "Success",
-        description: "Successfully created creditor.",
-        action: (
-          <ToastAction altText="View Creditors">View Creditors</ToastAction>
-        ),
-      });
-    },
-    onError: (data) => {
-      toast({
-        variant: "destructive",
-        title: "Something went wrong : " + data.name,
-        description: data.message,
-        duration: 5000,
-      });
-    },
-  });
-
-  if (createCreditorMutation.isPending) {
-    return <Loading />;
-  }
 
   return (
     <div className="space-y-6">
@@ -54,7 +16,7 @@ export default function RegisterCreditor() {
         </p>
       </div>
       <Separator />
-      <RegisterForm createCreditor={createCreditorMutation} />
+      <RegisterForm/>
     </div>
   );
 }
