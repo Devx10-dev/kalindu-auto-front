372908c3f81d20f5dab0ebbae57d011da162d6a2
Avishka Sathyanjana <baavishka@gmail.com>
2024-06-12 02:02:36 +0530

    style: change the layout of the cash invoice and Bill Summery

--- src/pages/dashboard/invoice/cash/CashInvoice.tsx
+++ src/pages/dashboard/invoice/cash/CashInvoice.tsx
@@ -5,6 +5,11 @@ import OutsourcedItemDetails from "./components/OutSourcedItemDetails";
 import useCashInvoiceStore from "./context/useCashInvoiceStore";
 import CustomerDetails from "@/pages/dashboard/invoice/cash/components/CustomerDetails.tsx";
 import Commissions from "@/pages/dashboard/invoice/cash/components/Commisions.tsx";
+import {CardContent, CardHeader} from "@/components/ui/card.tsx";
+import PageHeader from "@/components/card/PageHeader.tsx";
+import React from "react";
+
+import ReceiptIcon from "@/components/icon/ReceiptIcon";
 
 const CashInvoiceBase: React.FC = () => {
   // const axiosPrivate = useAxiosPrivate();
@@ -13,21 +18,42 @@ const CashInvoiceBase: React.FC = () => {
 
   return (
     <div className="mb-20">
-      <h2 className="text-2xl font-bold mb-4">Create Cash Invoice</h2>
+        <CardHeader>
+            <PageHeader
+                title="Cash Invoice"
+                description=""
+                icon={<ReceiptIcon height="30" width="28" color="#162a3b" />}
+            />
+        </CardHeader>
+        <CardContent
+            style={{
+                display: "flex",
+                gap: "10px",
+            }}>
 
-      <CustomerDetails />
-      <AddItem />
-      <InvoiceTable />
-      <Commissions />
-      {hasOutsourcedItems && (
-        <div className="pb-[350px]">
-          <OutsourcedItemDetails />
-        </div>
-      )}
+            <div
+                style={{
+                    flex: 9
+                }}
+            >
+                <CustomerDetails/>
+                <AddItem/>
+                <InvoiceTable/>
+                <Commissions/>
+                {hasOutsourcedItems && (
+                    <div className="pb-[350px]">
+                        <OutsourcedItemDetails/>
+                    </div>
+                )}
+            </div>
 
-      <div className="fixed bottom-0 bg-slate-200 border">
-        <BillSummary />
-      </div>
+            <div style={{
+                flex: 3
+            }}>
+                <BillSummary />
+            </div>
+        </CardContent>
+
     </div>
   );
 };
--- src/pages/dashboard/invoice/cash/components/BillSummary.tsx
+++ src/pages/dashboard/invoice/cash/components/BillSummary.tsx
@@ -1,175 +1,214 @@
-import React, { useEffect, useMemo } from "react";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Delete, Printer } from "lucide-react";
+import React, {useEffect, useMemo} from "react";
+import {Input} from "@/components/ui/input";
+import {Label} from "@/components/ui/label";
+import {Card, CardContent} from "@/components/ui/card";
+import {Button} from "@/components/ui/button";
+import {Delete, Printer} from "lucide-react";
 import useInvoiceStore from "../context/useCashInvoiceStore";
 import useAxiosPrivate from "@/hooks/usePrivateAxios";
-import { useToast } from "@/components/ui/use-toast.ts";
-import { CashInvoiceService } from "@/service/invoice/cashInvoiceApi.ts";
+import {useToast} from "@/components/ui/use-toast.ts";
+import {CashInvoiceService} from "@/service/invoice/cashInvoiceApi.ts";
+import {OptionalLabel} from "@/components/formElements/FormLabel.tsx";
 
 const BillSummary = () => {
-  const {
-    invoiceItemDTOList,
-    discountPercentage,
-    setDiscountPercentage,
-    discountAmount,
-    setDiscountAmount,
-    vatPercentage,
-    setVatPercentage,
-    vatAmount,
-    setVatAmount,
-    setTotalPrice,
-    getRequestData,
-  } = useInvoiceStore();
+    const {
+        invoiceItemDTOList,
+        discountPercentage,
+        setDiscountPercentage,
+        discountAmount,
+        setDiscountAmount,
+        vatPercentage,
+        setVatPercentage,
+        vatAmount,
+        setVatAmount,
+        setTotalPrice,
+        getRequestData,
+    } = useInvoiceStore();
 
-  const axiosPrivate = useAxiosPrivate();
-  const cashInvoiceService = new CashInvoiceService(axiosPrivate);
+    const axiosPrivate = useAxiosPrivate();
+    const cashInvoiceService = new CashInvoiceService(axiosPrivate);
 
-  /*
-    const subtotal = invoiceItemDTOList.reduce(
-        (acc: any, item: any) => acc + item.quantity * item.price - item.quantity * item.discount,
-        0
+    /*
+      const subtotal = invoiceItemDTOList.reduce(
+          (acc: any, item: any) => acc + item.quantity * item.price - item.quantity * item.discount,
+          0
+      );
+
+      const discountedTotal = subtotal - (discountAmount || 0);
+      const totalWithVat = discountedTotal + (vatAmount || 0);
+      // setTotalPrice(totalWithVat);
+  */
+    const subtotal = useMemo(() => {
+        return invoiceItemDTOList.reduce(
+            (acc: any, item: any) =>
+                acc + item.quantity * item.price - item.quantity * item.discount,
+            0,
+        );
+    }, [invoiceItemDTOList]);
+
+    const discountedTotal = useMemo(
+        () => subtotal - (discountAmount || 0),
+        [subtotal, discountAmount],
+    );
+    const totalWithVat = useMemo(
+        () => discountedTotal + (vatAmount || 0),
+        [discountedTotal, vatAmount],
     );
 
-    const discountedTotal = subtotal - (discountAmount || 0);
-    const totalWithVat = discountedTotal + (vatAmount || 0);
-    // setTotalPrice(totalWithVat);
-*/
-  const subtotal = useMemo(() => {
-    return invoiceItemDTOList.reduce(
-      (acc: any, item: any) =>
-        acc + item.quantity * item.price - item.quantity * item.discount,
-      0,
+    // Update the total price when discountedTotal or vatAmount changes
+    useEffect(() => {
+        setTotalPrice(totalWithVat);
+    }, [totalWithVat, setTotalPrice]);
+
+    const handleDiscountPercentageChange = (
+        e: React.ChangeEvent<HTMLInputElement>,
+    ) => {
+        const percentage = Math.max(parseFloat(e.target.value), 0);
+        setDiscountPercentage(percentage);
+        setDiscountAmount((subtotal * percentage) / 100);
+    };
+
+    const handleDiscountAmountChange = (
+        e: React.ChangeEvent<HTMLInputElement>,
+    ) => {
+        const amount = Math.max(parseFloat(e.target.value), 0);
+        setDiscountAmount(amount);
+        setDiscountPercentage((amount / subtotal) * 100);
+    };
+
+    const handleVatPercentageChange = (
+        e: React.ChangeEvent<HTMLInputElement>,
+    ) => {
+        const percentage = Math.max(parseFloat(e.target.value), 0);
+        setVatPercentage(percentage);
+        setVatAmount((discountedTotal * percentage) / 100);
+    };
+
+    const handleVatAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const amount = Math.max(parseFloat(e.target.value), 0);
+        setVatAmount(amount);
+        setVatPercentage((amount / discountedTotal) * 100);
+    };
+
+    const {toast} = useToast();
+
+    async function printAndSaveInvoice() {
+        if (invoiceItemDTOList.length === 0) {
+            return toast({
+                title: "No items added to the invoice",
+                description: "",
+                variant: "destructive",
+            });
+        }
+
+        try {
+            const requestData = getRequestData();
+            console.log(requestData);
+            const createdInvoice =
+                await cashInvoiceService.createCashInvoice(requestData);
+            console.log("Cash invoice created:", createdInvoice);
+            // Handle success response, such as printing the invoice or displaying a success message
+        } catch (error) {
+            console.error("Error creating cash invoice:", error);
+            // Handle error
+        }
+    }
+
+    return (
+        <Card>
+            <CardContent className="p-5 shadow-sm pt-0">
+                <h3 className="text-2xl font-semibold leading-none tracking-tight mb-4">
+                    Bill Summary
+                </h3>
+                <div className="mt-8">
+                    <div className="d-flex justify-between mb-4">
+                        <Label>Discount (%)</Label>
+                        <Input
+                            style={{
+                                maxWidth: "100px",
+                                textAlign: "right",
+                                padding: 2,
+                                maxHeight: 30,
+                            }}
+                            type="number"
+                            value={discountPercentage}
+                            onChange={handleDiscountPercentageChange}
+                            min={0}
+                            max={100}
+                        />
+                    </div>
+                    <div className="d-flex justify-between mb-4">
+                        <Label>Discount Amount (LKR)</Label>
+                        <Input
+                            style={{
+                                maxWidth: "100px",
+                                textAlign: "right",
+                                padding: 2,
+                                maxHeight: 30,
+                            }}
+                            type="number"
+                            value={discountAmount}
+                            onChange={handleDiscountAmountChange}
+                        />
+                    </div>
+                    <div className="d-flex justify-between mb-4">
+                        <Label>VAT Percentage (%)</Label>
+                        <Input
+                            style={{
+                                maxWidth: "100px",
+                                textAlign: "right",
+                                padding: 2,
+                                maxHeight: 30,
+                            }}
+                            type="number"
+                            value={vatPercentage}
+                            onChange={handleVatPercentageChange}
+                            min={0}
+                            max={100}
+                        />
+                    </div>
+                    <div className="d-flex justify-between mb-4">
+                        <Label>VAT Amount (LKR)</Label>
+                        <Input
+                            style={{
+                                maxWidth: "100px",
+                                textAlign: "right",
+                                padding: 2,
+                                maxHeight: 30,
+                            }}
+
+                            type="number"
+                            value={vatAmount}
+                            onChange={handleVatAmountChange}
+                        />
+                    </div>
+
+                </div>
+                <div className="flex justify-start text-left mt-16">
+                    <div className="text-left">
+                        <p className="text-xl font-semibold bg-slate-200 text-slate-900 pl-4 pt-2 pb-2 pr-4 rounded-md">
+                            Total : LKR {totalWithVat.toFixed(2)}
+                        </p>
+                        <div className="d-flex">
+                            <Button
+                                className="mt-4 mb-3"
+                                onClick={() => printAndSaveInvoice()}
+                            >
+                                <Printer className={"mr-2"}/>
+                                Print Invoice
+                            </Button>
+                            <Button
+                                className="mt-4 mb-3 bg-red-400 ml-2 text-white">
+                                <Delete className={"mr-2"}/> Cancel
+                            </Button>
+                        </div>
+
+                    </div>
+
+                </div>
+            </CardContent>
+        </Card>
     );
-  }, [invoiceItemDTOList]);
-
-  const discountedTotal = useMemo(
-    () => subtotal - (discountAmount || 0),
-    [subtotal, discountAmount],
-  );
-  const totalWithVat = useMemo(
-    () => discountedTotal + (vatAmount || 0),
-    [discountedTotal, vatAmount],
-  );
-
-  // Update the total price when discountedTotal or vatAmount changes
-  useEffect(() => {
-    setTotalPrice(totalWithVat);
-  }, [totalWithVat, setTotalPrice]);
-
-  const handleDiscountPercentageChange = (
-    e: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    const percentage = Math.max(parseFloat(e.target.value), 0);
-    setDiscountPercentage(percentage);
-    setDiscountAmount((subtotal * percentage) / 100);
-  };
-
-  const handleDiscountAmountChange = (
-    e: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    const amount = Math.max(parseFloat(e.target.value), 0);
-    setDiscountAmount(amount);
-    setDiscountPercentage((amount / subtotal) * 100);
-  };
-
-  const handleVatPercentageChange = (
-    e: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    const percentage = Math.max(parseFloat(e.target.value), 0);
-    setVatPercentage(percentage);
-    setVatAmount((discountedTotal * percentage) / 100);
-  };
-
-  const handleVatAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const amount = Math.max(parseFloat(e.target.value), 0);
-    setVatAmount(amount);
-    setVatPercentage((amount / discountedTotal) * 100);
-  };
-
-  const { toast } = useToast();
-
-  async function printAndSaveInvoice() {
-    if (invoiceItemDTOList.length === 0) {
-      return toast({
-        title: "No items added to the invoice",
-        description: "",
-        variant: "destructive",
-      });
-    }
-
-    try {
-      const requestData = getRequestData();
-      console.log(requestData);
-      const createdInvoice =
-        await cashInvoiceService.createCashInvoice(requestData);
-      console.log("Cash invoice created:", createdInvoice);
-      // Handle success response, such as printing the invoice or displaying a success message
-    } catch (error) {
-      console.error("Error creating cash invoice:", error);
-      // Handle error
-    }
-  }
-
-  return (
-    <Card className="w-full">
-      <CardContent className="p-3 shadow-sm  bg-slate-200 w-full">
-        <h2 className="text-xl font-bold mb-8">Bill Summary</h2>
-        <div className="grid grid-cols-7 gap-4">
-          <div className="flex flex-col gap-2">
-            <Label>Discount Percentage (%)</Label>
-            <Input
-              type="number"
-              value={discountPercentage}
-              onChange={handleDiscountPercentageChange}
-              placeholder="Discount Percentage"
-            />
-          </div>
-          <div className="flex flex-col gap-2">
-            <Label>Discount Amount (LKR)</Label>
-            <Input
-              type="number"
-              value={discountAmount}
-              onChange={handleDiscountAmountChange}
-              placeholder="Discount Amount"
-            />
-          </div>
-          <div className="flex flex-col gap-2">
-            <Label>VAT Percentage (%)</Label>
-            <Input
-              type="number"
-              value={vatPercentage}
-              onChange={handleVatPercentageChange}
-              placeholder="VAT Percentage"
-            />
-          </div>
-          <div className="flex flex-col gap-2">
-            <Label>VAT Amount (LKR)</Label>
-            <Input
-              type="number"
-              value={vatAmount}
-              onChange={handleVatAmountChange}
-              placeholder="VAT Amount"
-            />
-          </div>
-          <span className="text-xl bg-slate-200 text-slate-900 p-5 rounded-md">
-            Total : LKR {totalWithVat.toFixed(2)}
-          </span>
-          <Button className="mt-4 mb-5" onClick={() => printAndSaveInvoice()}>
-            <Printer className={"mr-2"} /> Print Invoice
-          </Button>
-          <Button className="mt-4 mb-5 bg-red-400 ml-2 text-black">
-            <Delete className={"mr-2"} /> Cancel
-          </Button>
-        </div>
-        <div className="flex justify-start text-left mt-4">
-          <div className="text-left"></div>
-        </div>
-      </CardContent>
-    </Card>
-  );
 };
 
 export default BillSummary;
