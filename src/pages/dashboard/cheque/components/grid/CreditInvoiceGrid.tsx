1b26d6efd300df0b19400d974316a9a1ea278d69
Harsha Gunawardane <137129344+Harsha-Gunawardane@users.noreply.github.com>
2024-09-18 07:32:23 +0530

    Fix height of cheque invoice view modal

--- src/pages/dashboard/cheque/components/grid/CreditInvoiceGrid.tsx
+++ src/pages/dashboard/cheque/components/grid/CreditInvoiceGrid.tsx
@@ -26,39 +26,47 @@ function CreditInvoiceGrid({
   };
 
   return (
-    <>
-      <Table className="border rounded-md text-md mb-5 table-responsive">
-        <TableCaption>Settled Credit Invoice Details</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Invoice Id</TableHead>
-            <TableHead style={{ textAlign: "end" }}>Total Price</TableHead>
-            <TableHead>Issued Time</TableHead>
-            <TableHead style={{ textAlign: "end" }}>Settled Amount</TableHead>
-          </TableRow>
-        </TableHeader>
+    <div>
+      <div
+        className="py-4 bg-muted/50"
+        style={{ height: "450px", overflowY: "scroll", borderRadius: "5px" }}
+      >
+        <Table className="border rounded-md text-md mb-4 pt-0 mt-0 table-responsive">
+          <TableCaption>Settled Credit Invoice Details</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Invoice Id</TableHead>
+              <TableHead>Issued Time</TableHead>
+              <TableHead style={{ textAlign: "end" }}>Total Price</TableHead>
+              <TableHead style={{ textAlign: "end" }}>Settled Amount</TableHead>
+            </TableRow>
+          </TableHeader>
 
-        <TableBody>
-          {cheque &&
-            cheque.chequeInvoices.map((chequeInvoice) => (
-              <TableRow key={Math.random() * 100}>
-                <TableCell className="font-medium">
-                  {chequeInvoice.invoiceNo}
-                </TableCell>
-                <TableCell align="right">{chequeInvoice.totalPrice}</TableCell>
-                <TableCell>{`${chequeInvoice.issuedTime[0]}-${chequeInvoice.issuedTime[1]}-${chequeInvoice.issuedTime[2]} ${chequeInvoice.issuedTime[3]}:${chequeInvoice.issuedTime[4]}:${chequeInvoice.issuedTime[5]}`}</TableCell>
-                <TableCell align="right">{chequeInvoice.amount}</TableCell>
-              </TableRow>
-            ))}
-        </TableBody>
-      </Table>
+          <TableBody>
+            {cheque &&
+              cheque.chequeInvoices.map((chequeInvoice) => (
+                <TableRow key={Math.random() * 100}>
+                  <TableCell className="font-medium">
+                    {chequeInvoice.invoiceNo}
+                  </TableCell>
+                  <TableCell>{`${chequeInvoice.issuedTime[0]}-${chequeInvoice.issuedTime[1]}-${chequeInvoice.issuedTime[2]} ${chequeInvoice.issuedTime[3]}:${chequeInvoice.issuedTime[4]}:${chequeInvoice.issuedTime[5]}`}</TableCell>
 
-      <div className="d-flex justify-end">
+                  <TableCell align="right">
+                    {chequeInvoice.totalPrice}
+                  </TableCell>
+                  <TableCell align="right">{chequeInvoice.amount}</TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </div>
+
+      <div className="d-flex justify-end mt-6">
         <Button variant="outline" onClick={closeModal}>
           Close
         </Button>
       </div>
-    </>
+    </div>
   );
 }
 
