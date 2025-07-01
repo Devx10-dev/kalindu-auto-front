624c477d0507790602cbc43051a5244ba3796b66
harshag <137129344+Harsha-Gunawardane@users.noreply.github.com>
2025-07-01 22:29:49 +0530

    Update the cash invoice layout component

--- src/pages/dashboard/invoice/cash/components/BillSummary.tsx
+++ src/pages/dashboard/invoice/cash/components/BillSummary.tsx
@@ -19,6 +19,7 @@ import IconCash from "@/components/icon/IconCash";
 import CurrencyComponent from "../../view/components/CurrencyComponent";
 import { Switch } from "@/components/ui/switch";
 import { Separator } from "@/components/ui/separator";
+import PrintInvoice from "../../components/PrintInvoice";
 
 const BillSummary = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -53,18 +54,18 @@ const BillSummary = () => {
     return invoiceItemDTOList.reduce(
       (acc: any, item: any) =>
         acc + item.quantity * item.price - item.quantity * item.discount,
-      0,
+      0
     );
   }, [invoiceItemDTOList]);
 
   const discountedTotal = useMemo(
     () => subtotal - (discountAmount || 0),
-    [subtotal, discountAmount],
+    [subtotal, discountAmount]
   );
 
   const totalWithVat = useMemo(
     () => discountedTotal + (vatAmount || 0),
-    [discountedTotal, vatPercentage, vatAmount],
+    [discountedTotal, vatPercentage, vatAmount]
   );
 
   // Update the total price when discountedTotal or vatAmount changes
@@ -73,7 +74,7 @@ const BillSummary = () => {
   }, [totalWithVat, setTotalPrice]);
 
   const handleDiscountPercentageChange = (
-    e: React.ChangeEvent<HTMLInputElement>,
+    e: React.ChangeEvent<HTMLInputElement>
   ) => {
     const percentage = Math.max(parseFloat(e.target.value), 0);
     setDiscountPercentage(percentage);
@@ -81,7 +82,7 @@ const BillSummary = () => {
   };
 
   const handleDiscountAmountChange = (
-    e: React.ChangeEvent<HTMLInputElement>,
+    e: React.ChangeEvent<HTMLInputElement>
   ) => {
     const amount = Math.max(parseFloat(e.target.value), 0);
     setDiscountAmount(amount);
@@ -89,7 +90,7 @@ const BillSummary = () => {
   };
 
   const handleVatPercentageChange = (
-    e: React.ChangeEvent<HTMLInputElement>,
+    e: React.ChangeEvent<HTMLInputElement>
   ) => {
     const percentage = Math.max(parseFloat(e.target.value), 0);
     setVatPercentage(percentage);
@@ -193,10 +194,8 @@ const BillSummary = () => {
       title: "Print Invoice",
       description: "Confirm print invoice",
       content: (
-        <PrintCashInvoice
-          buttonRef={printButtonRef}
-          invoiceData={invoiceData}
-        />
+
+        <PrintInvoice buttonRef={printButtonRef} invoiceData={invoiceData} />
       ),
       execute: () => {
         if (printButtonRef.current) {
@@ -212,7 +211,7 @@ const BillSummary = () => {
     setVatIncluded(checked);
     if (checked) {
       setVatPercentage(
-        vatPercentage > 0 ? vatPercentage : defaultVatPercentage,
+        vatPercentage > 0 ? vatPercentage : defaultVatPercentage
       );
     } else {
       setVatPercentage(0);
