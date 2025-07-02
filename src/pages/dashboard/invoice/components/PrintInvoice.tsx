347f1940a7ff6284721f6352db22586e8cc202bb
Nipuna Rahal <Nipuna.Rahal@cambio.lk>
2025-07-02 09:36:19 +0530

    Added vehicle number input

--- src/pages/dashboard/invoice/components/PrintInvoice.tsx
+++ src/pages/dashboard/invoice/components/PrintInvoice.tsx
@@ -105,7 +105,7 @@ function PrintInvoice({
       newLine +
       "\x1B\x24\x3C\x00" +
       "\x1B\x4A\x08" +
-      (invoiceData?.vehicle || "") +
+      (invoiceData?.vehicle ? invoiceData?.vehicle : (vehicleNumber || "")) +
       "\x1B\x24\x7D\x01" +
       "\x1B\x61\x08" +
       " ".repeat(6) +
